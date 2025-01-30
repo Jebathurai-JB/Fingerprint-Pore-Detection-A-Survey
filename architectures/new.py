b7cb064c49ea9a73ec86630e3d6bac94bffa3f4c
JBee... <jebathurai2511999@gmail.com>
2025-01-30 10:21:24 +0530

    Update new.py

--- architectures/new.py
+++ architectures/new.py
@@ -7,10 +7,10 @@ class Net13NoMax(nn.Module):
 
         self.l1 = LayerBlock(1, numberFeatures, 5, False) # 13 -> 9
         #self.l2 = LayerBlock(numberFeatures, numberFeatures, 3, False) # 11 -> 9
-        self.l3 = LayerBlock(numberFeatures, numberFeatures, 3, False) # 9 -> 7
-        self.l4 =  LayerBlock(numberFeatures, numberFeatures, 3, False) # 7 -> 5 
-        self.l5 = LayerBlock(numberFeatures, numberFeatures, 3, False)  #5 -> 3
-        self.l6 = nn.Conv2d(numberFeatures, 1, kernel_size=3, padding="valid", stride=(1, 1))
+        self.l2 = LayerBlock(numberFeatures, numberFeatures, 3, False) # 9 -> 7
+        self.l3 =  LayerBlock(numberFeatures, numberFeatures, 3, False) # 7 -> 5 
+        self.l4 = LayerBlock(numberFeatures, numberFeatures, 3, False)  #5 -> 3
+        self.l5 = nn.Conv2d(numberFeatures, 1, kernel_size=3, padding="valid", stride=(1, 1))
         self.sigmoid = nn.Sigmoid() 
 
         self.numberLayers = 7
@@ -20,10 +20,10 @@ class Net13NoMax(nn.Module):
         
         x = self.l1(x)
         #x = self.l2(x)
+        x = self.l2(x)
         x = self.l3(x)
         x = self.l4(x)
         x = self.l5(x)
-        x = self.l6(x)
         x = self.sigmoid(x)
 
         return x
