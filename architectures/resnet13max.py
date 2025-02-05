e3d11929e66fa68aa60a9120683c42e094d5e0fb
JBee... <jebathurai2511999@gmail.com>
2025-02-05 13:01:52 +0530

    Update resnet13max.py

--- architectures/resnet13max.py
+++ architectures/resnet13max.py
@@ -5,22 +5,24 @@ class ResNet13Max(nn.Module):
     def __init__(self, numberFeatures):
         super(ResNet13Max, self).__init__()
         self.l1 = LayerBlock(1, numberFeatures, 5, True)    # 15 -> 11
-        #self.l2 = LayerBlock(numberFeatures, numberFeatures, 3, True)   # 11 -> 7
-        self.l3 = nn.Conv2d(numberFeatures, 1, kernel_size=3, padding="valid", stride=(1, 1))
-        self.l4 = nn.MaxPool2d(kernel_size=3, stride=(1, 1))        
-        self.sigmoid = nn.Sigmoid() 
-        self.numberLayers = 4
-
+        self.l2 = LayerBlock(numberFeatures, numberFeatures, 3, True)   # 11 -> 7
+        
+        self.l3 = nn.Conv2d(numberFeatures, numberFeatures, kernel_size=3, padding=1, stride=1)
+        self.l4 = nn.Conv2d(numberFeatures, 1, kernel_size=3, padding=1, stride=1)
+        
+        self.l5 = nn.MaxPool2d(kernel_size=2, stride=2)  # Downsamples feature map
+        
+        self.sigmoid = nn.Sigmoid()
+        self.numberLayers = 5
 
     def forward(self, x):
-         
         x = self.l1(x)
-         
-       # x = x[:,:,self.l2.resValue:-self.l2.resValue,self.l2.resValue:-self.l2.resValue] + self.l2(x)
-                  
+        x = self.l2(x)  # Reintroduced second LayerBlock
+        
         x = self.l3(x)
         x = self.l4(x)
-         
+        
+        x = self.l5(x)  # Pooling after convolutions
+        
         x = self.sigmoid(x)
         return x
-
