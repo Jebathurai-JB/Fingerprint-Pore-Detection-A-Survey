00d1ab44ae512e671b00b2abc7b8632dc408b7d0
JBee... <jebathurai2511999@gmail.com>
2025-02-06 10:42:47 +0530

    Update net13nomax.py

--- architectures/net13nomax.py
+++ architectures/net13nomax.py
@@ -5,7 +5,7 @@ class Net13NoMax(nn.Module):
         super(Net13NoMax, self).__init__()
 
         self.l1 = nn.Sequential(
-            nn.Conv2d(1, numberFeatures, kernel_size=5, padding=2, stride=1),  # Only this layer has padding
+            nn.Conv2d(1, numberFeatures, kernel_size=5, padding=1, stride=1),  # Only this layer has padding
             nn.BatchNorm2d(numberFeatures),
             nn.ReLU()
         )
@@ -27,12 +27,20 @@ class Net13NoMax(nn.Module):
             nn.BatchNorm2d(numberFeatures),
             nn.ReLU()
         )
-
-        self.l5 = nn.Conv2d(numberFeatures, 1, kernel_size=3, stride=1)
+        
+        self.l5 = nn.Sequential(
+            nn.Conv2d(numberFeatures, numberFeatures, kernel_size=3, stride=1),
+            nn.BatchNorm2d(numberFeatures),
+            nn.ReLU()
+        )
+        
+        self.l6 = nn.Conv2d(numberFeatures, 1, kernel_size=3, stride=1)
 
         self.sigmoid = nn.Sigmoid()
 
         self.numberLayers = 5  # Updated based on the modifications
+
+    
 
     def forward(self, x):
         x = self.l1(x)
@@ -45,6 +53,9 @@ class Net13NoMax(nn.Module):
         print(x.shape)
         x = self.l5(x)
         print(x.shape)
+        x = self.l6(x)
+        print(x.shape)
         x = self.sigmoid(x)
+        
 
         return x
